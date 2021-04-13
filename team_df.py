eea01c9cd58e44c9746b8cdb8c6440182165a1ee
Nick Ramsey <nramseysc@gmail.com>
2021-04-12 22:55:25 -0700

    track cumulative runs scored over games played

--- team_df.py
+++ team_df.py
@@ -13,7 +13,7 @@ sea_df['date'] = sea_df['date'].dt.strftime('%m/%d/%Y')
 
 # Add Home and Away Team fields
 home_team = []
-road_team = []
+away_team = []
 
 for ind in sea_df.index:
     if sea_df['winner'][ind] == 'Home':
@@ -22,11 +22,27 @@ for ind in sea_df.index:
         home_team.append(sea_df['losing_abbr'][ind])
 
     if sea_df['winner'][ind] == 'Away':
-        road_team.append(sea_df['winning_abbr'][ind])
+        away_team.append(sea_df['winning_abbr'][ind])
     else:
-        road_team.append(sea_df['losing_abbr'][ind])
+        away_team.append(sea_df['losing_abbr'][ind])
 
 sea_df.insert(81, 'home_team', home_team, True)
-sea_df.insert(82, 'road_team', road_team, True)
+sea_df.insert(82, 'away_team', away_team, True)
 
+sea_df['team'] = 'SEA'
+
+# Add Cumulative Runs Scored Column
+team_runs = []
+for ind in sea_df.index:
+    if sea_df['away_team'][ind] == 'SEA':
+        team_runs.append(sea_df['away_runs'][ind])
+    else:
+        team_runs.append(sea_df['home_runs'][ind])
+
+sea_df.insert(83, 'team_runs', team_runs, True)
+
+sea_df['cumulative_runs'] = sea_df['team_runs'].cumsum()
+
+sea_df = sea_df[['date', 'winning_abbr', 'away_runs',
+                 'home_runs', 'away_team', 'home_team', 'team', 'team_runs', 'cumulative_runs']]
 print(sea_df)
